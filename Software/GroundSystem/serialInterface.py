24b28a6f9a521e642e7142f5a5ffdde46e709254
millist097 <millist.097@gmail.com>
2019-07-18 13:20:10 -0400

    modified serialInterface to run a thread that handles the serial port reading

--- Software/GroundSystem/serialInterface.py
+++ Software/GroundSystem/serialInterface.py
@@ -1,61 +1,159 @@
-
 from tkinter import *
 from tkinter import scrolledtext
 from tkinter import scrolledtext
 from tkinter import messagebox
 
 from tkinter.ttk import *
-import serial
+import serial as Serial
 import serial.tools.list_ports
 import threading
+import time
+import random
+import queue as Queue
 
+class GuiPart:
+	
 
-ser = serial.Serial()
+			
+	def __init__(self, master, queue, ser, endCommand):
+		self.queue = queue
+		# Set up the GUI
+		
+		
+		portList = Combobox(master, state="readonly")
+		ports = serial.tools.list_ports.comports()
+		portList['values']=ports
+		portList.current(0)
+		portList.place(y=40,x=10, width=150)
+		
+		baudRateCombo = Combobox(master, state="readonly")
+		baudRateCombo['values'] = [9600,115200]
+		baudRateCombo.current(0)
+		baudRateCombo.place( y = 40, x = 170, width = 80)
+		
+		def openPort():
+			if ser.is_open:
+				ser.close()
+				self.txt.insert(INSERT,'Port closed\n')
+				
+			temp = portList.get()
+			temp = temp.split(' ')
+			self.txt.insert(INSERT, temp[0] + ' - Opening Port\n')
+			ser.port = temp[0]
+			ser.baudrate = 9600
+			ser.open()
+			#ser.flushInput()
+			if ser.is_open :
+				self.txt.insert(INSERT, 'Port is Open \n')
+			else:
+				self.txt.insert(INSERT, 'Port failed to Open\n')
+				
+		def updatePortList():
+			ports = serial.tools.list_ports.comports()
+			portList['values']=ports
+			
+		openPort_btn = Button(master, text='Open Port',command=openPort)
+		openPort_btn.place(y=10,x=10)
+		
+		getPorts = Button(master, text='Find Ports', command=updatePortList)
+		getPorts.place( x = 90, y=10)
+		
+		self.txt = scrolledtext.ScrolledText(master,width=40,height=15)
+		self.txt.place(y=60,x=10)
+		
+		def on_closing():
+			if messagebox.askokcancel("Quit", "Do you want to quit?"):
+				master.destroy()
+				if(ser.is_open):
+					ser.close()
+				endCommand()
+		master.protocol("WM_DELETE_WINDOW",on_closing)
+		# Add more GUI stuff here depending on your specific needs
 
+		
+		
+	def processIncoming(self):
+		"""Handle all messages currently in the queue, if any."""
+		while self.queue.qsize(  ):
+			try:
+				msg = self.queue.get(0)
+				# Check contents of message and do whatever is needed. As a
+				# simple test, print it (in real life, you would
+				# suitably update the GUI's display in a richer fashion).
+				self.txt.insert(INSERT, msg)
+				#print(msg)
+			except Queue.Empty:
+				# just on general principles, although we don't
+				# expect this branch to be taken in this case
+				pass
 
+class ThreadedClient:
+	"""
+	Launch the main part of the GUI and the worker thread. periodicCall and
+	endApplication could reside in the GUI part, but putting them here
+	means that you have all the thread controls in a single place.
+	"""
+	def __init__(self, master):
+		"""
+		Start the GUI and the asynchronous threads. We are in the main
+		(original) thread of the application, which will later be used by
+		the GUI as well. We spawn a new thread for the worker (I/O).
+		"""
+		self.master = master
+
+		# Create the queue
+		self.queue = Queue.Queue(  )
+		
+		# Create Serial instance
+		self.ser = Serial.Serial(timeout=1)
+		
+		# Set up the GUI part
+		self.gui = GuiPart(master, self.queue, self.ser, self.endApplication)
+
+		# Set up the thread to do asynchronous I/O
+		# More threads can also be created and used, if necessary
+		self.running = 1
+		self.thread1 = threading.Thread(target=self.workerThread1)
+		self.thread1.start(  )
+
+		# Start the periodic call in the GUI to check if the queue contains
+		# anything
+		self.periodicCall(  )
+
+	def periodicCall(self):
+		"""
+		Check every 200 ms if there is something new in the queue.
+		"""
+		self.gui.processIncoming(  )
+		if not self.running:
+			# This is the brutal stop of the system. You may want to do
+			# some cleanup before actually shutting it down.
+			import sys
+			sys.exit(1)
+		self.master.after(200, self.periodicCall)
+
+	def workerThread1(self):
+		"""
+		This is where we handle the asynchronous I/O. For example, it may be
+		a 'select(  )'. One important thing to remember is that the thread has
+		to yield control pretty regularly, by select or otherwise.
+		"""
+		while self.running:
+
+			if self.ser.is_open:
+				#print(self.ser.port)
+				time.sleep(.010)
+				msg = self.ser.readline()
+				stringMsg = msg.decode("utf-8")
+				self.queue.put(stringMsg)
+
+	def endApplication(self):
+		self.running = 0
+
+rand = random.Random(  )
 window = Tk()
 window.title("Serial port Interface")
 window.geometry('350x450')
 
-combo = Combobox(window)
-ports = serial.tools.list_ports.comports()
-combo['values']=ports
-combo.current(0)
-combo.place(y=40,x=10)
-
-
-
-def clicked():
-	temp = combo.get()
-	temp = temp.split(' ')
-	txt.insert(INSERT, temp[0] + ' - Opening Port\n')
-	ser.port = temp[0]
-	ser.baudrate = 9600
-	ser.open()
-	if ser.is_open :
-		txt.insert(INSERT, 'Port is Open \n')
-	else:
-		txt.insert(INSERT, 'Port failed to Open\n')
-		
-btn = Button(window, text='Open Port',command=clicked)
-btn.place(y=10,x=10)
-
-def testPort():
-	txt.insert(INSERT,ser.is_open)
-	
-btn2 = Button(window, text='Test Port', command = testPort)
-btn2.place(y=10,x=135)
-
-
-txt = scrolledtext.ScrolledText(window,width=40,height=15)
-txt.place(y=60,x=10)
-
-
-
-def on_closing():
-	if messagebox.askokcancel("Quit", "Do you want to quit?"):
-		window.destroy()
-		if(ser.is_open):
-			ser.close()
-window.protocol("WM_DELETE_WINDOW",on_closing)
-window.mainloop()
+client = ThreadedClient(window)
+window.mainloop(  )
